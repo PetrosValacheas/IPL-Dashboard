9e0f9be6021aa9d0ae1c97e0a99bca69c3e76a5c
PetrosVal <petrosva12gmail.com>
2021-05-22 16:43:13 +0300

    Create and match model

---
+++ src/main/java/io/pe21/ipldashboard/data/MatchDataProcessor.java
@@ -0,0 +1,49 @@
+package io.pe21.ipldashboard.data;
+
+import java.time.LocalDate;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.item.ItemProcessor;
+
+import io.pe21.ipldashboard.model.Match;
+
+public class MatchDataProcessor implements ItemProcessor<MatchInput, Match> {
+
+    private static final Logger log = LoggerFactory.getLogger(MatchDataProcessor.class);
+
+    @Override
+    public Match process(final MatchInput matchInput) throws Exception {
+
+        Match match = new Match();
+        match.setId(Long.parseLong(matchInput.getId()));
+        match.setCity(matchInput.getCity());
+
+        match.setDate(LocalDate.parse(matchInput.getDate()));
+        match.setPlayerOfMatch(matchInput.getVenue());
+        match.setVenue(matchInput.getVenue());
+
+        String firstInningTeam, secondInningsTeam;
+
+        if ("bat".equals(matchInput.getToss_decision())) {
+            firstInningTeam = matchInput.getToss_winner();
+            secondInningsTeam = matchInput.getToss_winner().equals(matchInput.getTeam1()) ? matchInput.getTeam2()
+                    : matchInput.getTeam1();
+        } else {
+            secondInningsTeam = matchInput.getToss_winner();
+            firstInningTeam = matchInput.getToss_winner().equals(matchInput.getTeam1()) ? matchInput.getTeam2()
+                    : matchInput.getTeam1();
+        }
+        match.setTeam1(firstInningTeam);
+        match.setTeam2(secondInningsTeam);
+
+        match.setTossWinner(matchInput.getToss_winner());
+        match.setTossDecision(matchInput.getToss_decision());
+        match.setResult(matchInput.getResult());
+        match.setResultMargin(matchInput.getResult_margin());
+        match.setUmpire1(matchInput.getUmpire1());
+        match.setUmpire2(matchInput.getUmpire2());
+
+        return match;
+    }
+}
